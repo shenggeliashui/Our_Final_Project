77910be89e46a8c3cc1b415b1fffa5f6ba47e3f7
mchale <denglzh23@mail2.sysu.edu.cn>
2024-06-20 15:41:09 +0800

    个人中心添加到路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import UserLogin from '../components/UserLogin.vue';
 import RegisterUser from '../components/RegisterUser.vue'; // 导入注册组件
+import UserProfile from '../views/UserProfile.vue';
 
 const routes = [
   {
@@ -19,6 +20,11 @@ const routes = [
     name: 'HomePage',
     component: HomePage,
     meta: { requiresAuth: true }
+  },
+  {
+    path: '/profile',
+    name: 'UserProfile',
+    component: UserProfile
   }
 ];
 
@@ -27,6 +33,7 @@ const router = createRouter({
   routes
 });
 
+// 如果路由需要身份验证（meta: { requiresAuth: true }）并且没有令牌，用户将被重定向到登录页面。
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token');
   if (to.matched.some(record => record.meta.requiresAuth) && !token) {
