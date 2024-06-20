351e59682608bca7bae58a8a75ce3009c4dbf270
mchale <denglzh23@mail2.sysu.edu.cn>
2024-06-20 11:55:11 +0800

    保护路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import UserLogin from '../components/UserLogin.vue';
+import RegisterUser from '../components/RegisterUser.vue'; // 导入注册组件
 
 const routes = [
   {
@@ -9,15 +10,30 @@ const routes = [
     component: UserLogin
   },
   {
+    path: '/register',
+    name: 'Register',
+    component: RegisterUser // 添加注册路由
+  },
+  {
     path: '/home',
     name: 'HomePage',
-    component: HomePage
+    component: HomePage,
+    meta: { requiresAuth: true }
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  if (to.matched.some(record => record.meta.requiresAuth) && !token) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
