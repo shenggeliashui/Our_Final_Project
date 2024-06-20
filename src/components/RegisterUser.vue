490155ef2307a57745a687c5974300e0e53a6418
mchale <denglzh23@mail2.sysu.edu.cn>
2024-06-20 11:41:05 +0800

    提交注册表单时，数据发送到后端

--- src/components/RegisterUser.vue
+++ src/components/RegisterUser.vue
@@ -28,6 +28,8 @@
 </template>
 
 <script>
+import api from '../services/api';
+
 export default {
   name: 'RegisterUser',
   data() {
@@ -75,24 +77,102 @@ export default {
         delete this.errors.confirmPassword;
       }
     },
-    handleRegister() {
+    async handleRegister() {
       this.validateUsername();
       this.validateEmail();
       this.validatePassword();
       this.validateConfirmPassword();
 
       if (Object.keys(this.errors).length === 0) {
-        // 在这里处理注册逻辑，比如发送请求到服务器创建新用户
-        console.log('Username:', this.username);
-        console.log('Email:', this.email);
-        console.log('Password:', this.password);
-        // 假设注册成功，重定向到登录页面
-        this.$router.push('/');
+        try {
+          const response = await api.post('/register', {
+            username: this.username,
+            email: this.email,
+            password: this.password
+          });
+
+          console.log('注册成功:', response.data);
+          // 注册成功后重定向到登录页面
+          this.$router.push('/');
+        } catch (error) {
+          console.error('注册失败:', error.response.data);
+          // 处理后端返回的错误信息
+          if (error.response.data.errors) {
+            this.errors = error.response.data.errors;
+          }
+        }
       }
     }
   }
 };
 </script>
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'RegisterUser',-->
+<!--  data() {-->
+<!--    return {-->
+<!--      username: '',-->
+<!--      email: '',-->
+<!--      password: '',-->
+<!--      confirmPassword: '',-->
+<!--      errors: {}-->
+<!--    };-->
+<!--  },-->
+<!--  methods: {-->
+<!--    validateUsername() {-->
+<!--      if (!this.username) {-->
+<!--        this.errors.username = '用户名不能为空';-->
+<!--      } else {-->
+<!--        delete this.errors.username;-->
+<!--      }-->
+<!--    },-->
+<!--    validateEmail() {-->
+<!--      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;-->
+<!--      if (!this.email) {-->
+<!--        this.errors.email = '电子邮件不能为空';-->
+<!--      } else if (!emailPattern.test(this.email)) {-->
+<!--        this.errors.email = '电子邮件格式不正确';-->
+<!--      } else {-->
+<!--        delete this.errors.email;-->
+<!--      }-->
+<!--    },-->
+<!--    validatePassword() {-->
+<!--      if (!this.password) {-->
+<!--        this.errors.password = '密码不能为空';-->
+<!--      } else if (this.password.length < 6) {-->
+<!--        this.errors.password = '密码长度不能少于6位';-->
+<!--      } else {-->
+<!--        delete this.errors.password;-->
+<!--      }-->
+<!--    },-->
+<!--    validateConfirmPassword() {-->
+<!--      if (!this.confirmPassword) {-->
+<!--        this.errors.confirmPassword = '请确认密码';-->
+<!--      } else if (this.confirmPassword !== this.password) {-->
+<!--        this.errors.confirmPassword = '密码不匹配';-->
+<!--      } else {-->
+<!--        delete this.errors.confirmPassword;-->
+<!--      }-->
+<!--    },-->
+<!--    handleRegister() {-->
+<!--      this.validateUsername();-->
+<!--      this.validateEmail();-->
+<!--      this.validatePassword();-->
+<!--      this.validateConfirmPassword();-->
+
+<!--      if (Object.keys(this.errors).length === 0) {-->
+<!--        // 在这里处理注册逻辑，比如发送请求到服务器创建新用户-->
+<!--        console.log('Username:', this.username);-->
+<!--        console.log('Email:', this.email);-->
+<!--        console.log('Password:', this.password);-->
+<!--        // 假设注册成功，重定向到登录页面-->
+<!--        this.$router.push('/');-->
+<!--      }-->
+<!--    }-->
+<!--  }-->
+<!--};-->
+<!--</script>-->
 
 <!--<script>-->
 <!--export default {-->
@@ -158,7 +238,7 @@ button {
 }
 
 button:hover {
-  background-color: lightsalmon;
+  background-color: coral;
 }
 
 .error-message {
