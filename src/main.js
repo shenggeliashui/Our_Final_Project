5a2dd446b834e4bc9ebe19a1c7c73b092b537677
mchale <denglzh23@mail2.sysu.edu.cn>
2024-06-20 15:50:41 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-// import store from './store'; // 如果使用 Vuex
+import store from './store'; // 使用 Vuex
 
 createApp(App)
   .use(router)
-  // .use(store) // 如果使用 Vuex
+  .use(store) // 使用 Vuex
   .mount('#app');
 
