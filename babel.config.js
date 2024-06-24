94771086a8261dc379c5d51ae17a5444638c028d
mchale <denglzh23@mail2.sysu.edu.cn>
2024-06-24 09:23:11 +0800

    测试的配置：jest、babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }]
-  ]
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    node: "current"
+                }
+            }
+        ]
+    ],
+  "plugins": [
+      "@babel/plugin-transform-modules-commonjs"
+    ]
 };
--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,12 @@
 //   preset: '@vue/cli-plugin-unit-jest'
 // }
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/default',
+  preset: '@vue/cli-plugin-unit-jest/',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\.js$': 'babel-jest' // 处理 JavaScript 文件
   },
+  "transformIgnorePatterns": ['node_modules/(?!(axios)/)'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   testEnvironment: 'jsdom'
 };
